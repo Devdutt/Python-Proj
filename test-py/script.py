05d7184463d22b2d74760ef8b940173d6e0601df
Badger519 <dave.mohapatra@nytimes.com>
2019-07-22 16:11:11 -0400

    Format Doc tested

--- test-py/script.py
+++ test-py/script.py
@@ -1,11 +1,13 @@
 import sys
 import os
 import requests
+import math
+
 
 print(sys.version)
 print(sys.executable)
 
-r = requests.get("https://www.google.com")
-
+r = requests.get("https://www.python.org")
 print(r.status_code)
+
 # end comment
