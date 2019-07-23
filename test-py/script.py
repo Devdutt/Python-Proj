3357127752c81d475bf4c0faf5079b5403013504
Badger519 <dave.mohapatra@nytimes.com>
2019-07-23 17:00:02 -0400

    added input

--- test-py/script.py
+++ test-py/script.py
@@ -3,11 +3,12 @@ import os
 import requests
 import math
 
-
 print(sys.version)
 print(sys.executable)
 
 r = requests.get("https://www.google.com")
 print(r.status_code)
 
-# end comment
+
+nam = input('Who are you?')
+print('Welcome ' + nam)
