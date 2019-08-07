593d443546d77947f1c5f514e553f3ec48381cc8
Badger519 <dave.mohapatra@nytimes.com>
2019-08-07 11:58:50 -0400

    String operations

--- test-py/strings.py
+++ test-py/strings.py
@@ -24,7 +24,7 @@ print(logical)
 
 raw_data = "dmarc=fail (p=NONE sp=NONE dis=NONE) header.from=nytimes.com Date: Thu, 01 Aug 2019 12:18:30 -0700 (PDT) Received: from app06.cl02.ca.us.workfront.com  (app06.cl02.ca.us.workfront.com [10.44.102.36]) by mail.attask-ondemand.com (Postfix) with ESMTP id 5265C81CBA for <Dave.Mohapatra@nytimes.com>; Thu, 1 Aug 2019 13:18:29 -0600 (MDT)"
 
-pos_1 = raw_data.find("<")
+pos_1 = raw_data.find("@")
 print(pos_1)
 pos_2 = raw_data.find(">", pos_1)
 
@@ -35,8 +35,8 @@ print("the length of data is:", len(raw_data))
 counter = 0
 for i in raw_data:
     pos_a = raw_data.find("ESMTP")
-    pos_b = raw_data.find("", pos_a)
+    pos_b = raw_data.find(" ", pos_a)
 
-r_value = raw_data[pos_a:5]
-print(pos_b)
-print(r_value)
+r_value = raw_data[pos_a:pos_b]
+print("pos b is ", pos_b)
+print("r value is ", r_value)
