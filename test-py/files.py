4eaa71aa50349b7d3e285b47bd925ed7e13529f2
Badger519 <dave.mohapatra@nytimes.com>
2019-08-15 17:52:01 -0400

    removed string loops

--- test-py/files.py
+++ test-py/files.py
@@ -1,8 +1,7 @@
 file_handle = open("../data/junk-data.txt", "r")
-data_dump = file_handle.read()
-count = 0
-for lines in file_handle:
-    count = count + lines
-print("Number of lines in the data file: ", count)
-print("Is the file closed? ", file_handle.closed)
-# print(data_dump)
+
+for lns in file_handle:
+    lns = lns.rstrip()
+    if not "ads" in lns:
+        continue
+    print(lns)
